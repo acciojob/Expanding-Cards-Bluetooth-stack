4da6ea8193fa52b6882b374a29531fe8cc529ea2
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-03-21 01:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const pic = document.querySelectorAll('.panel');
+
+pic.forEach((photo)=>{
+	photo.addEventListener('click',()=>{
+		removeActive();
+		photo.classList.add('active');
+	});
+});
+
+function removeActive(){
+	pic.forEach((photo)=>{
+		photo.classList.remove('active');
+	});
+}
